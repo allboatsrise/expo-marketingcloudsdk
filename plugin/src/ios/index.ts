897bab6ae395a23652fedc397f0de1ae17eaaf7b
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2022-01-30 23:14:31 -0500

    Implemented iOS config

--- plugin/src/ios/index.ts
+++ plugin/src/ios/index.ts
@@ -1,25 +1,193 @@
+import fs from 'fs'
+import path from 'path'
 import {
   ConfigPlugin,
-  withInfoPlist,
+  withAppDelegate,
+  withDangerousMod,
+  withEntitlementsPlist,
 } from '@expo/config-plugins';
 
 import { MarketingCloudSdkPluginProps } from '../types';
+import { getProjectName } from '@expo/config-plugins/build/ios/utils/Xcodeproj';
+import { mergeContents } from '@expo/config-plugins/build/utils/generateCode';
 
 export const withIOSConfig: ConfigPlugin<MarketingCloudSdkPluginProps> = (
   config,
   props
 ) => {
-  config = withInfoPlist(config, (config) => {
-    config.modResults['SFMC_APP_ID'] = props.appId;
-    config.modResults['SFMC_ACCESS_TOKEN'] = props.accessToken;
-    config.modResults['SFMC_SERVER_URL'] = props.serverUrl;
-    if (typeof props.analyticsEnabled === 'boolean') config.modResults['SFMC_ANALYTICS_ENABLED'] = props.analyticsEnabled ? 1 : 0;
-    if (typeof props.inboxEnabled === 'boolean') config.modResults['SFMC_INBOX_ENABLED'] = props.inboxEnabled ? 1 : 0;
-    if (typeof props.locationEnabled === 'boolean') config.modResults['SFMC_LOCATION_ENABLED'] = props.locationEnabled ? 1 : 0;
-    if (props.mid) config.modResults['SFMC_MID'] = props.mid;
-    if (props.senderId) config.modResults['SFMC_SENDER_ID'] = props.senderId;
-    return config;
-  })
+  config = withEntitlements(config, props)
+
+  config = withExtraAppDelegateProtocols(config, props)
+
+  config = withConfiguration(config, props)
+  config = withPushConfiguration(config, props)
+  config = withDelegateImplementation(config, props)
 
   return config;
 };
+
+const withEntitlements: ConfigPlugin<MarketingCloudSdkPluginProps> = (config, {mode = 'development'}) => {
+  return withEntitlementsPlist(config, async (config) => {
+    config.modResults['aps-environment'] = mode;
+    return config;
+  });
+}
+
+const withExtraAppDelegateProtocols: ConfigPlugin<MarketingCloudSdkPluginProps> = (config) => {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const appDelegateHeaderFile = path.join(config.modRequest.platformProjectRoot, getProjectName(config.modRequest.projectRoot), 'AppDelegate.h')
+
+      let headerContent = fs.readFileSync(appDelegateHeaderFile, 'utf8');
+
+      ['UIApplicationDelegate', 'UNUserNotificationCenterDelegate'].forEach(protocol => {
+        if (headerContent.match(new RegExp(`${protocol}`))) return config
+
+        headerContent = headerContent.replace(/(EXAppDelegateWrapper\s*<.*?RCTBridgeDelegate.*?)(>)/, `$1, ${protocol}$2`)  
+      })
+
+      fs.writeFileSync(appDelegateHeaderFile, headerContent)
+
+      return config
+    }
+  ])
+}
+
+const withConfiguration: ConfigPlugin<MarketingCloudSdkPluginProps> = (config, props) => {
+  return withAppDelegate(config, async config => {
+    config.modResults.contents = mergeContents({
+      src: config.modResults.contents,
+      newSrc: `#import <MarketingCloudSDK/MarketingCloudSDK.h>`,
+      anchor: /#import "AppDelegate\.h"/,
+      offset: 1,
+      tag: '@allboatsrise/expo-marketingcloudsdk(header)',
+      comment: '//'
+    }).contents
+
+    config.modResults.contents = mergeContents({
+      src: config.modResults.contents,
+      newSrc: `  MarketingCloudSDKConfigBuilder *mcsdkBuilder = [MarketingCloudSDKConfigBuilder new];
+    [mcsdkBuilder sfmc_setApplicationId:@${JSON.stringify(props.appId)}];
+    [mcsdkBuilder sfmc_setAccessToken:@${JSON.stringify(props.accessToken)}];
+    [mcsdkBuilder sfmc_setAnalyticsEnabled:@(${props.analyticsEnabled ? 'YES' : 'NO'})];
+    [mcsdkBuilder sfmc_setMarketingCloudServerUrl:@${JSON.stringify(props.serverUrl)}];
+
+    NSError *error = nil;
+    BOOL success = [[MarketingCloudSDK sharedInstance] sfmc_configureWithDictionary:[mcsdkBuilder sfmc_build] error:&error];`,
+      anchor: /\[super application:application didFinishLaunchingWithOptions:launchOptions\];/,
+      offset: -1,
+      tag: '@allboatsrise/expo-marketingcloudsdk(configuration)',
+      comment: '//'
+    }).contents
+
+    return config;
+  })
+}
+
+const withPushConfiguration: ConfigPlugin<MarketingCloudSdkPluginProps> = (config, props) => {
+  return withAppDelegate(config, async config => {
+    config.modResults.contents = mergeContents({
+      src: config.modResults.contents,
+      newSrc: `  if (success == YES) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+          if (@available(iOS 10, *)) {
+              // set the UNUserNotificationCenter delegate - the delegate must be set here in
+              // didFinishLaunchingWithOptions
+              [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+              [[UIApplication sharedApplication] registerForRemoteNotifications];
+
+              [[UNUserNotificationCenter currentNotificationCenter]
+                  requestAuthorizationWithOptions:UNAuthorizationOptionAlert |
+                                                  UNAuthorizationOptionSound |
+                                                  UNAuthorizationOptionBadge
+                                completionHandler:^(BOOL granted, NSError *_Nullable error) {
+                                  if (error == nil) {
+                                      if (granted == YES) {
+                                          dispatch_async(dispatch_get_main_queue(), ^{
+                                                         });
+                                      }
+                                  }
+                                }];
+          } else {
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < 100000
+              UIUserNotificationSettings *settings = [UIUserNotificationSettings
+                  settingsForTypes:UIUserNotificationTypeBadge | UIUserNotificationTypeSound |
+                                   UIUserNotificationTypeAlert
+                        categories:nil];
+              [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+#endif
+              [[UIApplication sharedApplication] registerForRemoteNotifications];
+          }
+        });
+    } else {
+        //  MarketingCloudSDK sfmc_configure failed
+        os_log_debug(OS_LOG_DEFAULT, "MarketingCloudSDK sfmc_configure failed with error = %@",
+                     error);
+    }`,
+      anchor: /\[super application:application didFinishLaunchingWithOptions:launchOptions\];/,
+      offset: 1,
+      tag: '@allboatsrise/expo-marketingcloudsdk(push-configuration)',
+      comment: '//'
+    }).contents
+
+    return config;
+  })
+}
+
+const withDelegateImplementation: ConfigPlugin<MarketingCloudSdkPluginProps> = (config, props) => {
+  return withAppDelegate(config, async config => {
+    config.modResults.contents = mergeContents({
+      src: config.modResults.contents,
+      newSrc: `
+- (void)application:(UIApplication *)application
+    didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    [[MarketingCloudSDK sharedInstance] sfmc_setDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application
+    didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    os_log_debug(OS_LOG_DEFAULT, "didFailToRegisterForRemoteNotificationsWithError = %@", error);
+}
+
+// The method will be called on the delegate when the user responded to the notification by opening
+// the application, dismissing the notification or choosing a UNNotificationAction. The delegate
+// must be set before the application returns from applicationDidFinishLaunching:.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+    didReceiveNotificationResponse:(UNNotificationResponse *)response
+              withCompletionHandler:(void (^)(void))completionHandler {
+    // tell the MarketingCloudSDK about the notification
+    [[MarketingCloudSDK sharedInstance] sfmc_setNotificationRequest:response.notification.request];
+
+    if (completionHandler != nil) {
+        completionHandler();
+    }
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+        willPresentNotification:(UNNotification *)notification
+          withCompletionHandler:
+              (void (^)(UNNotificationPresentationOptions options))completionHandler {
+    NSLog(@"User Info : %@", notification.request.content.userInfo);
+    completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert |
+                      UNAuthorizationOptionBadge);
+}
+
+// This method is REQUIRED for correct functionality of the SDK.
+// This method will be called on the delegate when the application receives a silent push
+- (void)application:(UIApplication *)application
+    didReceiveRemoteNotification:(NSDictionary *)userInfo
+          fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    [[MarketingCloudSDK sharedInstance] sfmc_setNotificationUserInfo:userInfo];
+
+    completionHandler(UIBackgroundFetchResultNewData);
+}`.trim(),
+      anchor: /-\s*\(BOOL\)application:\(UIApplication\s*\*\)application\s*didFinishLaunchingWithOptions/,
+      offset: -1,
+      tag: '@allboatsrise/expo-marketingcloudsdk(user-notification-methods)',
+      comment: '//'
+    }).contents
+
+    return config;
+  })
+}
--- plugin/src/types.ts
+++ plugin/src/types.ts
@@ -5,11 +5,19 @@ export type MarketingCloudSdkPluginProps = {
   accessToken: string;
   /** Marketing Cloud server url */
   serverUrl: string;
-  /** Marketing Cloud FCM sender id */
+  /**
+   * (Android only) Marketing Cloud FCM sender id. Defaults to `project_info.project_number`
+   * defined in `android.googleServicesFile` (google-services.json) if defined.
+   */
   senderId?: string;
   /** Marketing Cloud mid */
   mid?: string;
   inboxEnabled?: boolean;
   locationEnabled?: boolean;
   analyticsEnabled?: boolean;
+  /**
+   * (iOS only) Environment of the app: either 'development' or 'production'. Defaults to 'development'.
+   * @default 'development'
+   */
+  mode?: 'development' | 'production';
 };
