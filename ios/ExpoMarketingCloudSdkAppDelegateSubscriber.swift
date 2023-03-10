595b72a8b27114e55594e8993ef220fccdb831ea
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2023-03-10 16:35:07 -0500

    add delegate

---
+++ ios/ExpoMarketingCloudSdkAppDelegateSubscriber.swift
@@ -0,0 +1,6 @@
+import ExpoModulesCore
+
+public class ExpoMarketingCloudSdkAppDelegateSubscriber : ExpoAppDelegateSubscriber {
+  public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    return false
+  }
