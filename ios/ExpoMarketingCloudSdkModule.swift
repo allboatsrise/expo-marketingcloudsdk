850c3769eccd52e8bca8cad0313a4f9ffa2db1a2
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2023-03-12 21:40:56 -0400

    add helpers

--- ios/ExpoMarketingCloudSdkModule.swift
+++ ios/ExpoMarketingCloudSdkModule.swift
@@ -10,26 +10,7 @@ public class ExpoMarketingCloudSdkModule: Module {
     // The module will be accessible from `requireNativeModule('ExpoMarketingCloudSdk')` in JavaScript.
     Name("ExpoMarketingCloudSdk")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants([
-      "PI": Double.pi
-    ])
-
     // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      return "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { (value: String) in
-      // Send an event to JavaScript.
-      self.sendEvent("onChange", [
-        "value": value
-      ])
-    }
+    Events("onLog")
   }
 }
