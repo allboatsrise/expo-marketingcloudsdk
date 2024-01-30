a45beda51ffc1ee926c00143d09dbf735367c253
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2024-01-30 15:13:45 -0500

    use `SFMCSdk.requestPushSdk` to get `mp` instead of `SFMCSdk.mp` directly

--- ios/ExpoMarketingCloudSdkModule.swift
+++ ios/ExpoMarketingCloudSdkModule.swift
@@ -41,70 +41,102 @@ public class ExpoMarketingCloudSdkModule: Module, ExpoMarketingCloudSdkLoggerDel
     }
 
     AsyncFunction("isPushEnabled") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.pushEnabled())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.pushEnabled())
+      }
     }
 
     AsyncFunction("enablePush") { (promise: Promise) in
-      SFMCSdk.mp.setPushEnabled(true)
-      promise.resolve(SFMCSdk.mp.pushEnabled())
+      SFMCSdk.requestPushSdk { mp in
+        mp.setPushEnabled(true)
+        promise.resolve(mp.pushEnabled())
+      }
     }
 
     AsyncFunction("disablePush") { (promise: Promise) in
-      SFMCSdk.mp.setPushEnabled(false)
-      promise.resolve(SFMCSdk.mp.pushEnabled())
+      SFMCSdk.requestPushSdk { mp in
+        mp.setPushEnabled(false)
+        promise.resolve(mp.pushEnabled())
+      }
     }
 
     AsyncFunction("getSystemToken") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.deviceToken())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.deviceToken())
+      }
     }
     
     AsyncFunction("setSystemToken") { (token: String, promise: Promise) in
       do {
         let token = try ExpoMarketingCloudSdkDeviceToken.init(hexString: token)
-        SFMCSdk.mp.setDeviceToken(token.data)
-        promise.resolve(SFMCSdk.mp.deviceToken())
+        
+        SFMCSdk.requestPushSdk { mp in
+          mp.setDeviceToken(token.data)
+          promise.resolve(mp.deviceToken())
+        }
       } catch {
         promise.reject("invalid-hex-token", "Failed to convert token to data type")
       }
     }
 
     AsyncFunction("getDeviceID") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.deviceIdentifier())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.deviceIdentifier())
+      }
     }
 
     AsyncFunction("getAttributes") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.attributes())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.attributes())
+      }
     }
 
     AsyncFunction("setAttribute") { (key: String, value: String, promise: Promise) in
       SFMCSdk.identity.setProfileAttribute(key, value)
-      promise.resolve(SFMCSdk.mp.attributes())
+      
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.attributes())
+      }
     }
 
     AsyncFunction("clearAttribute") { (key: String, promise: Promise) in
       SFMCSdk.identity.clearProfileAttribute(key: key)
-      promise.resolve(SFMCSdk.mp.attributes())
+      
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.attributes())
+      }
     }
 
     AsyncFunction("addTag") { (tag: String, promise: Promise) in
-      promise.resolve(SFMCSdk.mp.addTag(tag))
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.addTag(tag))
+      }
     }
 
     AsyncFunction("removeTag") { (tag: String, promise: Promise) in
-      promise.resolve(SFMCSdk.mp.removeTag(tag))
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.removeTag(tag))
+      }
     }
 
     AsyncFunction("getTags") { (promise: Promise) in
-      promise.resolve(Array(arrayLiteral: SFMCSdk.mp.tags()))
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(Array(arrayLiteral: mp.tags()))
+      }
     }
 
     AsyncFunction("setContactKey") { (contactKey: String, promise: Promise) in
       SFMCSdk.identity.setProfileId(contactKey)
-      promise.resolve(SFMCSdk.mp.contactKey())
+      
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.contactKey())
+      }
     }
 
     AsyncFunction("getContactKey") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.contactKey())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.contactKey())
+      }
     }
 
     AsyncFunction("getSdkState") { (promise: Promise) in
@@ -118,63 +150,90 @@ public class ExpoMarketingCloudSdkModule: Module, ExpoMarketingCloudSdkLoggerDel
     }
 
     AsyncFunction("deleteMessage") { (messageId: String, promise: Promise) in
-      promise.resolve(SFMCSdk.mp.markMessageWithIdDeleted(messageId: messageId))
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.markMessageWithIdDeleted(messageId: messageId))
+      }
     }
 
     AsyncFunction("getDeletedMessageCount") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.getDeletedMessagesCount())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.getDeletedMessagesCount())
+      }
     }
 
     AsyncFunction("getDeletedMessages") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.getDeletedMessages())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.getDeletedMessages())
+      }
     }
 
-    AsyncFunction("getMessageCount") { (promise: Promise) in      promise.resolve(SFMCSdk.mp.getAllMessagesCount())
+    AsyncFunction("getMessageCount") { (promise: Promise) in
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.getAllMessagesCount())
+      }
     }
 
     AsyncFunction("getMessages") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.getAllMessages())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.getAllMessages())
+      }
     }
 
     AsyncFunction("getReadMessageCount") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.getReadMessagesCount())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.getReadMessagesCount())
+      }
     }
 
     AsyncFunction("getReadMessages") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.getReadMessages())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.getReadMessages())
+      }
     }
 
     AsyncFunction("getUnreadMessageCount") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.getUnreadMessagesCount())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.getUnreadMessagesCount())
+      }
     }
 
     AsyncFunction("getUnreadMessages") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.getUnreadMessages())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.getUnreadMessages())
+      }
     }
 
     AsyncFunction("markAllMessagesDeleted") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.markAllMessagesDeleted())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.markAllMessagesDeleted())
+      }
     }
 
     AsyncFunction("markAllMessagesRead") { (promise: Promise) in
-      promise.resolve(SFMCSdk.mp.markAllMessagesRead())
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.markAllMessagesRead())
+      }
     }
 
     AsyncFunction("refreshInbox") { (promise: Promise) in
-      let successful = SFMCSdk.mp.refreshMessages()
-      if (successful == false) {
-        promise.resolve(false)
-      } else {
-        // resolve previous promise if one exists
-        self.refreshInboxPromise?.resolve(false)
-        
-        // queue latest promise
-        self.refreshInboxPromise = promise
+      SFMCSdk.requestPushSdk { mp in
+        let successful = mp.refreshMessages()
+        if (successful == false) {
+          promise.resolve(false)
+        } else {
+          // resolve previous promise if one exists
+          self.refreshInboxPromise?.resolve(false)
+          
+          // queue latest promise
+          self.refreshInboxPromise = promise
+        }
       }
     }
 
     AsyncFunction("setMessageRead") { (messageId: String, promise: Promise) in
-      promise.resolve(SFMCSdk.mp.markMessageWithIdRead(messageId: messageId))
+      SFMCSdk.requestPushSdk { mp in
+        promise.resolve(mp.markMessageWithIdRead(messageId: messageId))
+      }
     }
   }
   
