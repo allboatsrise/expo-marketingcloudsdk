8b9e02b6b64329a65935e55029d79922710c670e
Andreas Aretakis <andreas.aretakis@tre.se>
2025-04-08 17:45:48 +0200

    chore: remove logs

--- android/src/main/java/expo/modules/marketingcloudsdk/service/ExpoFirebaseMessagingService.kt
+++ android/src/main/java/expo/modules/marketingcloudsdk/service/ExpoFirebaseMessagingService.kt
@@ -1,6 +1,5 @@
 package expo.modules.marketingcloudsdk.service
 
-import android.util.Log
 import com.google.firebase.messaging.RemoteMessage
 import com.salesforce.marketingcloud.messages.push.PushMessageManager
 import com.salesforce.marketingcloud.sfmcsdk.SFMCSdk
@@ -8,19 +7,13 @@ import expo.modules.notifications.service.ExpoFirebaseMessagingService
 
 open class ExpoFirebaseMessagingService : ExpoFirebaseMessagingService() {
   override fun onMessageReceived(remoteMessage: RemoteMessage) {
-    Log.d("ExpoFirebaseMessagingService", "onMessageReceived called with remoteMessage: ${remoteMessage.messageId}")
-    
     if (PushMessageManager.isMarketingCloudPush(remoteMessage)) {
-      Log.d("ExpoFirebaseMessagingService", "Message is identified as Marketing Cloud Push")
-      
       SFMCSdk.requestSdk { sdk ->
         sdk.mp {
-          Log.d("ExpoFirebaseMessagingService", "Handling Marketing Cloud Push message")
           it.pushMessageManager.handleMessage(remoteMessage)
         }
       }
     } else {
-      Log.d("ExpoFirebaseMessagingService", "Message is not a Marketing Cloud Push, passing to super")
       super.onMessageReceived(remoteMessage)
     }
   }
