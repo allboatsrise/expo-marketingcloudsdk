ba447464e1820abdcfcd177e5710a3788d36ecb5
Juan Vilas <108366198+juan-vilas@users.noreply.github.com>
2026-03-13 07:58:27 +0100

    add index signare and optional chaining

--- src/ExpoMarketingCloudSdkModule.ts
+++ src/ExpoMarketingCloudSdkModule.ts
@@ -1,7 +1,9 @@
 import { NativeModule, requireNativeModule } from 'expo';
 import { ExpoMarketingCloudSdkModuleEvents } from './ExpoMarketingCloudSdk.types';
 
-declare class ExpoMarketingCloudSdkModule extends NativeModule<ExpoMarketingCloudSdkModuleEvents> {}
+declare class ExpoMarketingCloudSdkModule extends NativeModule<ExpoMarketingCloudSdkModuleEvents> {
+    [key: string]: any;
+}
 
 // This call loads the native module object from the JSI.
 export default requireNativeModule<ExpoMarketingCloudSdkModule>('ExpoMarketingCloudSdk');
